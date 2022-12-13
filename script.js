12472006468169b27cfe72a9abca48f91c1c6e04
Himynameismarco <marcozander94@web.de>
2022-12-13 18:22:59 +0100

    One can use the calculator for one equation

--- script.js
+++ script.js
@@ -1,32 +1,140 @@
-//let numbers = document.getElementsByClassName("number");
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 15; i++) {
   let number = document.getElementById(i.toString());
-  number.addEventListener('click', pressNumber);
+  number.addEventListener('click', pressButton);
 }
+
 
 let input = document.getElementById("input");
 let currentNum = 0;
 input.value = currentNum;
-let first = true;
+let firstPress = true;
+let arrayOfEquation = Array();
 
-function pressNumber(e) {
-  console.log("Pressed: " + e.target.id);
-
-  if (first) {
-    currentNum = e.target.id;
-    first = false;
-  } else {
-    let arrOfNums = currentNum.split('');
-    console.log(arrOfNums);
-    arrOfNums.push(e.target.id);
-    currentNum = "";
-    arrOfNums.forEach(element => (currentNum = currentNum + element));
+function pressButton(e) {
+  let pushedButton = e.target.id;
+  console.log("Pressed ID: " + e.target.id);
+  if (typeof parseInt(pushedButton) == 'number') {
+    createInputArrayAndShowOnInputField(e);
+    if (parseInt(pushedButton) == 14) {
+      analyzeEquation();
+    }
   }
   input.value = currentNum;
 }
 
 
+//TODO: split up in two functions
+function createInputArrayAndShowOnInputField(e) {
+  if (firstPress) {
+    currentNum = e.target.innerText;
+    firstPress = false;
+  } else {
+    arrayOfEquation = currentNum.split('');
+    arrayOfEquation.push(e.target.innerText);
+    currentNum = "";
+    arrayOfEquation.forEach(element => (currentNum = currentNum + element));
+    console.log("Original Array: " + arrayOfEquation);
+  }
+  input.value = currentNum;
+}
 
+function analyzeEquation() {
+  let arrayWithUsableArgs = splitArrayInArrayOfArgs();
+  let result = doOperation(arrayWithUsableArgs[1], arrayWithUsableArgs[0], arrayWithUsableArgs[2]);
+  console.log("Final Result: " + result);
+  arrayOfEquation.push(result);
+  currentNum = "";
+  arrayOfEquation.forEach(element => (currentNum = currentNum + element));
+  input.value = currentNum;
+}
+
+function splitArrayInArrayOfArgs() {
+  let currentArr = Array();
+  let resultArr = Array();
+  for (let i = 0; i < arrayOfEquation.length; i++) {
+    let elem = arrayOfEquation[i];
+    let argumentForResultArr;
+    let myOperation;
+    console.log("elem: " + elem);
+    //If it is not a number
+    if (isNaN(elem)) {
+      console.log("Element probably is an operation!");
+      myOperation = elem;
+      argumentForResultArr = evaluateArrToInt(currentArr);
+      resultArr.push(argumentForResultArr);
+      resultArr.push(elem);
+      currentArr = Array();
+    } else {
+      currentArr.push(elem);
+      console.log("currentArr: " + currentArr);
+    }
+  }
+  console.log("resultArr: " + resultArr);
+  return resultArr;
+}
+
+function evaluateArrToInt(arr) {
+  console.log("Starting with arr: " + arr);
+  let result = 0;
+  arr.forEach(element => (result = result + element));
+  console.log("result: " + result);
+  return parseInt(result);
+}
+
+function doOperation(operation, firstArg, secondArg) {
+  switch (operation) {
+    case "+":
+      console.log("Adding");
+      return add(firstArg, secondArg);
+      break;
+    case "-":
+      console.log("Subtracting");
+      return subtract(firstArg, secondArg);
+      break;
+    case "*":
+      console.log("Multiplying");
+      return multiply(firstArg, secondArg);
+      break;
+    case "/":
+      console.log("Dividing");
+      return divide(firstArg, secondArg);
+      break;
+  }
+}
+
+
+//operations:
+
+function add() {
+  let sum = 0.00;
+  for (let number = 0; number < arguments.length; number++) {
+    sum = sum + parseFloat(arguments[number]);
+  }
+  return sum;
+}
+function multiply() {
+  let product = 1.00;
+  for (let number = 0; number < arguments.length; number++) {
+    product = product * parseFloat(arguments[number]);
+  }
+  return product;
+}
+
+function subtract() {
+  let difference = parseFloat(arguments[0]);
+  for (let number = 1; number < arguments.length; number++) {
+    difference = difference - parseFloat(arguments[number]);
+  }
+  return difference;
+}
+
+function divide() {
+  let divisor = parseFloat(arguments[1]);
+  if (divisor === 0) {
+    return "You shall not divide through 0.";
+  }
+  return parseFloat(arguments[0]) / divisor;
+}
 
 
 
